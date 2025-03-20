f03082732d36b650cd7a2d7d32976e3c816b4f02
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2025-03-20 09:58:02 +0100

    .

--- OCHRONA_DANYCH/zad_1.py
+++ OCHRONA_DANYCH/zad_1.py
@@ -1,12 +1,19 @@
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
 codeType = ["abcdefghijklmnopqrstuwxyz0123456789",
             "0123456789abcdefghijklmnopqrstuwxyz",
             "abcdefghijklmnopqrstuqwxyzABCDEFGHIJKLMNOPQRSTUWXYZ"]
 to_code = "ASDASD"
 
- 
-move = 34
 
-print(len(to_code))
+f = open("Frankenstein.txt", "r", encoding="UTF-8")
+#print(f.read())
+ 
+to_code = f.read()
+
+#print(len(to_code))
 
 def Cesar(alphabet_idx, to_code, move, isDecoding):
     alphabet = codeType[alphabet_idx]
@@ -32,15 +39,49 @@ def Cesar(alphabet_idx, to_code, move, isDecoding):
             elif(i not in alphabet):
                 output+=i
                 break
-    print(output)
+    #print(output)
     return output
 
 
-def Cesar2():
-    print("WTF")
+coded = Cesar(0, to_code, 3, True)
 
-coded = Cesar(2, to_code, 3, True)
-decoded = Cesar(2, coded, 3, False)
+def pokazPlot(licznik): 
+    
+    litery = list(licznik.keys())  # Unikalne litery
+    wystapienia = list(licznik.values())  # Ich liczba wystąpień
+           
+    plt.figure(figsize=(10, 5))
+    plt.bar(litery, wystapienia, color='skyblue', edgecolor='black')
+
+    plt.xlabel("Litery")
+    plt.ylabel("Liczba wystąpień")
+    plt.title("Częstotliwość występowania liter w tekście")
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
+
+    plt.show()    
+#------------------------------------------------------
+
+def sprawdzCzestotliwosc(tekst):    
+    tekstDoLiczenia = tekst.lower()
+    licznik = collections.Counter(char for char in tekstDoLiczenia if char.isalpha())
+
+
+    #for litera, liczba in licznik.items(): 
+        #print(f"'{litera}': {liczba}")
+
+    suma_wszystkich_liter = sum(licznik.values())
+    czestotliwosci = {litera: liczba / suma_wszystkich_liter for litera, liczba in licznik.items()}
+    czestotliwosci_sum_kwadr = {litera: freq ** 2 for litera, freq in czestotliwosci.items()}
+    return (sum(czestotliwosci_sum_kwadr.values()))
+#------------------------------------------------------
+
+#for i in range (len(codeType)):
+for j in range(len(codeType[1])):
+    decoded = Cesar(0, coded, j, False)
+    r = sprawdzCzestotliwosc(decoded)
+    print(r, " --> ", j)
+#decoded = Cesar(1, coded, 3, False)
+#print(decoded)
 
 
     
